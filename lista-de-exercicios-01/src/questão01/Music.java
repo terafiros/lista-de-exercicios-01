cb3ff3f45611ad64b974bd75f73891f37cacf1ad
Eduardo Oliveira <Nayara@Nayara-PC>
2016-11-07 02:42:11 -0300

    Primeira versão da questão número 1, classes: MusicPlayer e Music

---
+++ lista-de-exercicios-01/src/questão01/Music.java
@@ -0,0 +1,48 @@
+package questão01;
+
+public class Music {
+	
+	private String name;
+	private String artist;
+	private String album;
+	private String duration;
+	
+	public Music(String name, String artist, String album, String duration) {
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+		this.duration = duration;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+}
---
+++ lista-de-exercicios-01/src/questão01/MusicPlayer.java
@@ -0,0 +1,74 @@
+package questão01;
+
+import java.util.List;
+import java.util.Random;
+
+public class MusicPlayer {
+	
+	private List<Music> musics;
+	private int atualMusic;
+	
+	public MusicPlayer(List<Music> musics){
+		this.musics = musics;
+		this.atualMusic = 0;
+	}
+	
+	public void play(){
+		displayDataOfMusic();
+	}
+	
+	private void displayDataOfMusic() {
+		Music atual = musics.get(atualMusic);
+		System.out.println(atual.getName());
+		System.out.println(atual.getArtist());
+		System.out.println(atual.getAlbum());
+		System.out.println(atual.getDuration());
+	}
+	
+	public void pause(){
+		System.out.println("Pausado");
+	}
+	
+	public void stop(){
+		System.out.println("Parado. Voltara da primeira música");
+		this.atualMusic = 0;
+	}
+	
+	public void nextMusic(){
+		if(isLastSong()){
+			this.atualMusic = 0;
+			play();
+		}
+		this.atualMusic++;
+		play();
+	}
+	
+	private boolean isLastSong() {
+		return this.atualMusic == musics.size()-1;
+	}
+	
+	public void previousMusic(){
+		if(isFirstSong()){
+			this.atualMusic = this.musics.size()-1;
+			play();
+		}
+	}
+	private boolean isFirstSong() {
+		return atualMusic == 0;
+	}
+	
+	public void reapeatAtualMusic(){
+		System.out.println("Repitindo música atual");
+	}
+	
+	public void reapeatAllMusics(){
+		System.out.println("Repetindo todas as músicas");
+	}
+	
+	public void playRamdom(){
+		this.atualMusic = (int)new Random().nextInt(this.musics.size());
+		play();
+	}
+	
+	
+}
